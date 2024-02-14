17854dccd329bc70a7c672a427657ea1609c1f96
Artyom <sgnoteii55@gmail.com>
2024-02-15 01:06:54 +0400

    chore | main updated

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.time.LocalDate;
-import java.util.Arrays;
+import task3.A3;
+import task3.B3;
+
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Scanner scanner = new Scanner(System.in);
         String input;
 
@@ -14,9 +14,9 @@ public class Main {
             System.out.print("""
                     > Введите номер задания:
                     [1] -- Сортировка данных в файле
-                    [2] -- TODO
+                    [2] -- Рекурсивное архивирование
                     [0] -- Выход
-                                        
+
                     """);
             System.out.print("> Вводите: ");
             input = scanner.nextLine();
@@ -30,13 +30,22 @@ public class Main {
                         String filename = scanner.nextLine();
                         if (filename.isBlank()) {
                             filename = "C:\\Users\\sgnot\\IdeaProjects\\Java Programming\\input.txt";
+                        } else if (!new File(filename).exists()) {
+                            throw new RuntimeException("Указанный файл не существует");
                         }
                         A3 taskClass = new A3(filename);
                         System.out.println("> Отсортированные строки файла: ");
                         taskClass.printEntries();
                         break;
                     case "2":
-                        System.out.println("> Тут пока что ничего нет...");
+                        System.out.print("> Введите относительный путь архивируемой директории: ");
+                        String path = scanner.nextLine();
+                        System.out.print("> Введите целевую строку: ");
+                        String targetString = scanner.nextLine();
+                        System.out.println("> Начало архивации");
+                        if (new B3(path, targetString).zipEntitiesWithNameContainingTargetString()) {
+                            System.out.println("> Архивация прошла успешно");
+                        }
                         break;
                     default:
                         System.out.println("[ERROR] Некорректный ввод. Попробуйте снова.");
